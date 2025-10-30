57fcb86d89a1afb158b25cdde755c59a60a7ee2a
Gee-sus <geesingh77@hotmail.com>
2025-10-30 23:16:03 +1300

    feat: create authentication screen with sign in and sign up forms

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,13 +1,131 @@
-import { KeyboardAvoidingView, Platform, View, Text } from "react-native";
-
+import { useAuth } from "@/lib/auth-context";
+import { useRouter } from 'expo-router';
+import { useState } from "react";
+import { KeyboardAvoidingView, Platform, StyleSheet, View } from "react-native";
+import { Button, Text, TextInput, useTheme } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function AuthScreen() {
-   
-    return <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} >
-        <View>
-            <Text>Create Account</Text>
+  const [isSignUp, setIsSignUp] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<string | null>("");
 
+  const theme = useTheme();
+
+  const router = useRouter();
+
+  const { signIn, signUp } = useAuth();
+
+  const handleAuth = async () => {
+    if (!email || !password) {
+      setError("Please fill in all fields");
+      return;
+    }
+
+    if (password.length < 6) {
+      setError("Password must be six characters long.");
+      return;
+    }
+
+    setError(null);
+
+    if (isSignUp) {
+     const error = await signUp(email, password);
+     if(error){
+      setError(error)
+      return
+     }
+     router.replace("/(tabs)");
+   } else {
+     const error = await signIn(email, password);
+     if(error){
+       setError(error)
+       return
+     }
+     router.replace("/(tabs)");
+   }
+ };
+
+  const handleSwitchMode = () => {
+    setIsSignUp((prev) => !prev);
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <View style={styles.content}>
+          <Text style={styles.title} variant="headlineMedium">
+            {isSignUp ? "Create Account" : "Welcome Back"}{" "}
+          </Text>
+
+          <TextInput
+            label="Email"
+            autoCapitalize="none"
+            keyboardType="email-address"
+            placeholder="example@gmail.com"
+            mode="outlined"
+            style={styles.input}
+            value={email}
+            onChangeText={setEmail}
+          />
+
+        <TextInput
+          label="Password"
+          autoCapitalize="none"
+          mode="outlined"
+          value={password}
+          secureTextEntry
+          style={styles.input}
+          onChangeText={setPassword}
+        />
+
+          {error && (
+            <Text style={{ color: theme.colors.error }}> {error} </Text>
+          )}
+
+          <Button mode="contained" style={styles.button} onPress={handleAuth}>
+            {isSignUp ? "Sign Up" : "Welcome Back"}
+          </Button>
+
+          <Button
+            mode="text"
+            onPress={handleSwitchMode}
+            style={styles.switchModeButton}
+          >
+            {isSignUp
+              ? "Already have an account? Sign in"
+              : "Don't have an account? Sign up"}
+          </Button>
         </View>
-
-    </KeyboardAvoidingView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#f5f5f5",
+  },
+  content: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+  },
+  title: {
+    textAlign: "center",
+    marginBottom: 24,
+  },
+  input: {
+    marginBottom: 16,
+  },
+  button: {
+    marginTop: 8,
+  },
+  switchModeButton: {
+    marginTop: 16,
+  },
+});
