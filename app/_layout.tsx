8e681311c732ec3f25be5cb823adf6b6b67d8790
Gee-sus <geesingh77@hotmail.com>
2025-10-30 23:16:18 +1300

    feat: add route guard for protected routes and authentication flow

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,28 +1,34 @@
-import { Stack, useRouter } from "expo-router";
+import { AuthProvider, useAuth } from "@/lib/auth-context";
+import { Stack, useRouter, useSegments } from "expo-router";
 import { useEffect } from "react";
 
-function RouteGuard({children}: {children: React.ReactNode}) {
-  const router = useRouter()
-  const isAuth = false;
-
+function RouteGuard({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const { user, isLoadingUser } = useAuth();
+  const segments = useSegments();
 
   useEffect(() => {
-    if (!isAuth) {
+    const inAuthGroup = segments[0] === "auth";
+    if (!user && !inAuthGroup && !isLoadingUser) {
       router.replace("/auth");
+    } else if (user && inAuthGroup && !isLoadingUser) {
+      router.replace("/(tabs)");
     }
-  }, [isAuth]);
-
+  }, [user, router, segments]);
+ 
+ 
   return <>{children}</>;
 }
 
 export default function Layout() {
   return (
-   <RouteGuard>
-    <Stack>
-      {/* <Stack.Screen name="auth" options={{ headerShown: false }} /> */}
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      
-    </Stack>
-  </RouteGuard>
+    <AuthProvider>
+      <RouteGuard>
+        <Stack>
+          <Stack.Screen name="auth" options={{ headerShown: false }} />
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </RouteGuard>
+    </AuthProvider>
   );
 }
